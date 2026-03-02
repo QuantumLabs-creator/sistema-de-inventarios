0b571c3e2d51f5a1aeec743862ed3dea92411ae9
QuantumLabs-creator <quantum.labs26@gmail.com>
2026-03-02 18:50:49 +0000

    update params en units

--- app/api/units/[id]/route.ts
+++ app/api/units/[id]/route.ts
@@ -1,5 +1,5 @@
 // app/api/units/[id]/route.ts
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { PrismaUnitRepository } from "@/src/modules/units/infrastructure/unit.repo";
 import { GetUnitUseCase } from "@/src/modules/units/application/getUnit.usecase";
@@ -8,10 +8,14 @@ import { DeleteUnitUseCase } from "@/src/modules/units/application/deleteUnit.us
 
 const repo = new PrismaUnitRepository();
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+){
   try {
+     const { id } = await params;
     const uc = new GetUnitUseCase(repo);
-    const row = await uc.execute(params.id);
+    const row = await uc.execute(id);
     return NextResponse.json(row);
   } catch (e: any) {
     return NextResponse.json(
@@ -22,13 +26,14 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
 }
 
 export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
     const body = await req.json();
     const uc = new UpdateUnitUseCase(repo);
-    const updated = await uc.execute(params.id, body);
+    const updated = await uc.execute(id, body);
     return NextResponse.json(updated);
   } catch (e: any) {
     return NextResponse.json(
@@ -39,12 +44,13 @@ export async function PATCH(
 }
 
 export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
+   _req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+    const { id } = await params;
     const uc = new DeleteUnitUseCase(repo);
-    await uc.execute(params.id);
+    await uc.execute(id);
     return NextResponse.json({ ok: true });
   } catch (e: any) {
     return NextResponse.json(
